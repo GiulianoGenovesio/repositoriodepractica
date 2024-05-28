58c0d50942ad8ff85f5898dcb7f59bc940f192a6
Micaela Maraschin <mica.maraschin@gmail.com>
2024-05-28 11:24:30 -0300

    agregué contenido de la clase

--- Clase14js/ejemplo01.js
+++ Clase14js/ejemplo01.js
@@ -1,9 +1,16 @@
 const nombre = prompt ('ingrese su nombre', '');
 const nota = parseInt (prompt ('ingrese su nota', ''));
 
+console.log (nombre);
+console.log (nota);
+
 if (Number.isNaN(nota)){
     document.write (`la nota ingresada no es valida`)
 } else if (nota >=4){
     document.write(`${nombre} estas aprobada/o con un ${nota}`);
     console.log(`${nombre} estas aprobada/o con un ${nota}`)
+}
+else{
+    document.write(`${nombre} estas desaprobada/o con un ${nota}`);
+    console.log (`${nombre} estas desaprobada/o con un ${nota}`);
 }
