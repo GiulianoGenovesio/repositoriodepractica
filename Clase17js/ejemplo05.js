1216f2e9817c3787ec3617904978ad359c6df728
Micaela Maraschin <mica.maraschin@gmail.com>
2024-06-18 11:45:15 -0300

    Empecé con el ejemplo05

---
+++ Clase17js/ejemplo05.js
@@ -0,0 +1,12 @@
+const nombre = `Giuliano`;
+const edad = 36;
+
+console.log(`hola! ${nombre} asi que vos tenés ${edad}`);
+
+const edad1 = 18;
+const edad2 = 32;
+
+const mensaje1 = `La cuota para personas ${edad1} es de 
+${edad1 >= 21 ? 5000 : 3500}`
+
+console.log(mensaje1)
