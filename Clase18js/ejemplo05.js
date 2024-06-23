a40062ca5117c0ada99ff16a7d2794d9841e7629
Micaela Maraschin <mica.maraschin@gmail.com>
2024-06-23 18:13:38 -0300

    Terminé con la clase, me faltó completar el ejercicio de js

--- Clase18js/ejemplo05.js
+++ Clase18js/ejemplo05.js
@@ -1,7 +1,23 @@
 // let now = new Date();
 // alert (now); // muestra en pantalla la fecha y la hora actuales
 
-const dia = new Date();
-const hours = dia.getHours();
+// const dia = new Date();
+// const hours = dia.getHours();
 
-console.log(hours);
+// console.log(hours);
+
+const nombre = prompt('Escribir tu nombre', '');
+const today = new Date();
+
+const hrs = today.getHours();
+
+if (hrs < 12) {
+    document.write('Buenos días ' + nombre + '!')
+    console.log(`Buenos días ${nombre} !!`)
+} else if (hrs >= 12 && hrs <= 19) {
+    document.write('Buenos tardes ' + nombre + '!')
+    console.log(`Buenos tardes ${nombre} !!`)
+} else {
+    document.write('Buenos noches ' + nombre + '!')
+    console.log(`Buenos noches ${nombre} !!`)
+}
