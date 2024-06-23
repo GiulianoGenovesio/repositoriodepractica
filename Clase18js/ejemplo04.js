c1eca23e4ca702b12cefaccd78caa7d35ba5840d
Micaela Maraschin <mica.maraschin@gmail.com>
2024-06-23 00:44:43 -0300

    ejemplo04

--- Clase18js/ejemplo04.js
+++ Clase18js/ejemplo04.js
@@ -7,3 +7,9 @@ console.log(b);
 
 [a, b, ...resto] = [10, 20, 30, 40, 50]
 console.log(resto);
+
+const vocales = ['a','e','i'];
+console.log(vocales);
+
+const vocalesCompletas = [...vocales,'o','u' ];
+console.log(vocalesCompletas);
